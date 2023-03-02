f4a1b7aa0638d9727d0ef0675d59b4fe249364b6
mavmaz <mariesmadrazo9@gmail.com>
2023-03-02 09:39:41 +0800

    change vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,4 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production' ? 'https://mariesvmadrazo.github.io/' : '/'
+})
